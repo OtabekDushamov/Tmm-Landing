b7daab854796e946c23c47357c85c3c33dab63fb
Otabek <otabek092004@gmail.com>
2024-11-20 21:07:16 +0500

    u;date

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['45.80.149.113', 'tmmcompany.uz', 'www.tmmcompany.uz']
 
-CSRF_TRUSTED_ORIGINS = ['http://45.80.149.113', 'https://ttmcompany.uz']
+CSRF_TRUSTED_ORIGINS = ['http://45.80.149.113', 'https://tmmcompany.uz']
 
 
 # Application definition
